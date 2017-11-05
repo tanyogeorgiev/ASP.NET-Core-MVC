9deadc1a275777ff026b048ca0175ff24c2d3319
Tanyo Georgiev <tanyogeorgiev@yahoo.com>
2017-11-05 13:25:31 +0200

    Implement some logic...

---
+++ 01.ASP.NET Core Introduction/FDMC/FDMC/Data/CatsDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FDMC.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class CatsDbContext : DbContext
+    {
+        public CatsDbContext(DbContextOptions<CatsDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Cat> Cats { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
